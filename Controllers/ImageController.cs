711f0619485285c8365ef4974e6e2c29a7a24464
anju <anjuj895@gmail.com>
2023-06-27 19:37:25 +0530

    image upload and display task

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,53 @@
+﻿using ImageUploadMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageUploadMVC.Controllers
+{
+    public class ImageController : Controller
+    {
+        //GET : Image
+
+        public ActionResult Index()
+        {
+            using (DbModels db = new DbModels())
+            {
+                return View(db.Images.ToList());
+            }
+
+            
+        }
+
+        [HttpGet]
+
+        public ActionResult Add()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Add(Image imageModel)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+            string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            imageModel.ImagePath = "~/Image/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/Image/"), fileName);
+            imageModel.ImageFile.SaveAs(fileName);
+            using (DbModels db = new DbModels())
+            {
+                db.Images.Add(imageModel);
+                db.SaveChanges();
+
+            }
+            ModelState.Clear();
+            return View();
+        }
+    }
+}
